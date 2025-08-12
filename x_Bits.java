cf358060dfc8d6e0dfc140dbcfa2c06111fe1423
TimKreutzkamp <tim.kreutzkamp@js-fin.de>
2025-08-12 11:51:29 +0200

    2nd

---
+++ x_Bits.java
@@ -0,0 +1,11 @@
+public class x_Bits {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i << 1);
+        System.out.println(i << 2);
+        System.out.println(i << 3);
+        System.out.println(i << 4);
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toBinaryString(i << 4));
+    }
+}
